5256d774623b55d4e3171d0e4b7bc9028b5f7b66
Tanuja-Ranjan <ranjantanuja08@gmail.com>
2025-01-22 06:09:22 +0000

    OperatorInJs

---
+++ 01_basics/lecture7.js
@@ -0,0 +1,44 @@
+//*************Operations***********//
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 ** 5);
+// console.log(2 / 3);
+// console.log(2 % 3);
+
+let str1 = "Hello "
+let str2 = "didi"
+
+// console.log(str1+str2);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( 3+ 4 * 5 % 3);   //5
+
+// console.log(true);  
+// console.log(+true);
+// console.log(true+); //not allowed
+// console.log(+"");
+
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
+//prefix postfix incremental operator assignment mdn
+
+
+
+
