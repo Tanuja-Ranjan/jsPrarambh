f2774b072877aab920e94dcd6b4a9c68d3015e0f
Tanuja-Ranjan <ranjantanuja08@gmail.com>
2025-01-22 05:15:51 +0000

    varialble declaration

---
+++ 01_basics/lecture4.js
@@ -0,0 +1,14 @@
+const accountId = 14453
+let accountEmail = "tanuja@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accounState
+
+// accountId = 34  //not allowed
+/*
+ prefer not to use var beacuse of issue in block scope and functional scope
+*/
+accountEmail = "ranjantanuja08@gamil.com"
+accountPassword = "8424418"
+accountCity = "Pune"
+console.table([accountId,accountEmail,accountPassword,accountCity,accounState]);
--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,2 @@
-console.log("pipi");
+
+console.log("Piki");
