911bbd709f033331cedc10eafbe72c61b5ed8e7e
Tanuja-Ranjan <ranjantanuja08@gmail.com>
2025-01-22 09:52:01 +0000

    Primitive and Non primitive data types

---
+++ 01_basics/lecture9.js
@@ -0,0 +1,43 @@
+/*
+
+Primitive ...call by value
+7 Types : String , Number, Boolean, Null, Undefined, Symbol, BigInt
+*/
+// const score = 100
+// const scoreValue = 100.3
+
+// const isLoggedIn = false
+// const outsideTemp = null
+// let userEmail;
+// const id = Symbol('123')
+// const anotherId = Symbol('123')
+// console.log(id === anotherId);
+
+const bigNumber = 12345n
+// console.log(typeof bigNumber);
+
+
+/*
+JavaScript ka bhai TypeScript
+Reference /Non-Primitive Data Types...Array, Objects, Functions 
+*/
+
+
+const heroes = ["Bhagat","Azad","Bose"]
+// console.log(typeof heroes);
+
+let myObj = {
+    name : "Tanu",
+    age : 15,
+}
+// console.log(typeof myObj);
+
+const myFunction = function(){
+    // console.log("Hello World");
+    
+}
+// console.log(typeof myFunction)
+/*
+Non-Primitive ke data types ko object hi bola jata hai , although function ke return type ko  object function khte hai 
+*/
+
