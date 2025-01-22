f8d1e67945cd3ac7ebcc0e9ba118281353b0da31
Tanuja-Ranjan <ranjantanuja08@gmail.com>
2025-01-22 05:24:06 +0000

    data_types

---
+++ lecture5.js
@@ -0,0 +1,12 @@
+"use strict"; //treat all js as newer version
+
+// alert( 3 + 3)   //we are using node js, not browser
+
+//code readability should be high
+
+//tc39.es ecma script original documentation
+
+
+let name = "Tanuja"
+let age = 20
+let isLoggedIn = false
