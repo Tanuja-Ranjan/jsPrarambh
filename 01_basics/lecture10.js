be9affe24066822396dff2701bdce85c2fabf5cf
Tanuja-Ranjan <ranjantanuja08@gmail.com>
2025-01-22 10:16:34 +0000

    Stack_Heap

---
+++ 01_basics/lecture10.js
@@ -0,0 +1,4 @@
+// Stack(Primitive), Heap(Non-Primitive)
+// jb v stack memory use hoti hai to jo bhi variable declare kiya hai uska copy milta hai
+// jb v heap me memory milti hai toh hme original value ka reference milta hi
+
