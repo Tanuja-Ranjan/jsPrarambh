0200d0fa1ca7f0cfc6d3aabe2698a363521d51e1
Tanuja-Ranjan <ranjantanuja08@gmail.com>
2025-01-22 05:41:04 +0000

    conversionOfDataTypes

---
+++ 01_basics/lecture6.js
@@ -0,0 +1,25 @@
+let score = null
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+
+let valueInNumber = Number (score)
+// console.log( valueInNumber);
+
+// For conversion in number 
+// "33" => 33
+//"33abc" => NaN ...not a number
+// true => 1 or false => 0
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+//1 => true
+//"" => false
+//"hitesh" =>true
+
+let integer = 33
+let stringNumber = String(integer)
+console.log( typeof stringNumber);
