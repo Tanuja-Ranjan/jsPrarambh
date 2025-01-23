72f1b8d041063d68d0d8b92cc63caf46a289eda7
Tanuja-Ranjan <ranjantanuja08@gmail.com>
2025-01-23 03:44:47 +0000

    arraypart1done

---
+++ 02_basics/array.js
@@ -0,0 +1,45 @@
+//array
+
+const myArr = [2, 6, 8, 9, 100, 123 ]
+const myHeroes = ["shaktiman", "nagraj"]
+
+const myArr2 = new Array(1, 2, 3, 4)
+// console.log(myArr[3]);
+
+//array methods
+// myArr.push(4)
+// console.log(myArr);
+// myArr.pop();
+// console.log(myArr);
+
+// myArr.unshift(57)
+// myArr.shift()
+// console.log(myArr);
+
+// console.log(myArr.includes(1));
+// console.log(myArr.indexOf(100));
+// console.log(myArr);
+
+const newArr = myArr.join()
+
+// console.log(myArr);
+// console.log(newArr);
+
+//slice, splice
+
+// console.log("A ",myArr);
+
+const myn1 = myArr.slice(1,5)
+
+// console.log(myn1);
+// console.log("B ",myArr);
+
+const myn2 = myArr.splice(1,5)
+// console.log(myn2);
+
+// console.log("C ",myArr);
+
+
+
+
+
