e4d8787ed8967f02a425d35e9818ca5f12b9a9bc
Tanuja-Ranjan <ranjantanuja08@gmail.com>
2025-01-25 07:29:50 +0000

    Scope of Function

---
+++ 03_basics/nestedScope_Closure.js
@@ -0,0 +1,40 @@
+function one(){
+    const username = "Tani"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+    
+        two()
+}
+
+// one()
+
+if(true){
+    const username = "talu"
+    if(username === "talu"){
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+    
+}
+
+// console.log(username);
+
+// +++++++++++++ interesting ++++++++++++
+
+console.log(addOne(5))
+function addOne(num){
+    return num + 1
+}
+
+// addOne(5)
+
+const addTwo = function(num){
+    return num + 2
+}
+
+console.log(addTwo(5))
