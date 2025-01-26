77fa3a093529813448e97acec36d75334a3f112c
Tanuja-Ranjan <ranjantanuja08@gmail.com>
2025-01-26 04:24:45 +0000

    if_else syntax

---
+++ 04_control_flow/one.js
@@ -0,0 +1,31 @@
+
+const balance = 1000
+
+// if(balance > 500) console.log("test"), console.log("test2");
+
+// if(balance < 500){
+//     console.log("less than 500"); 
+// } else if (balance < 750) {
+//     console.log("less than 750");
+    
+// }else if(balance < 900){
+//     console.log("less than 900");
+    
+// }else{
+//     console.log("less than 1200");
+    
+// }
+
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+if(userLoggedIn && debitCard){
+    console.log("Allow to buy a course");
+    
+}
+
+if(loggedInFromEmail || loggedInFromGoogle){
+    console.log("user logged in");
+    
+}
